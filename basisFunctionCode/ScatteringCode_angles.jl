23c9362a6c162baa33668adbfa409652339ed01c
edosim <99194376+edosim@users.noreply.github.com>
2025-05-14 14:50:03 +0100

    Added copyright notices to all files with code
    <file name> - Part of LJscatter
    
    Copyright (C) 2025 FieldingGroup
    Contact: h.h.fielding@ucl.ac.uk
    
    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
    
    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
    
    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.

--- basisFunctionCode/ScatteringCode_angles.jl
+++ basisFunctionCode/ScatteringCode_angles.jl
@@ -1,5 +1,17 @@
 #!/usr/bin/env julia
 
+"""
+ScatteringCode_angles.jl - Part of LJscatter
+Copyright (C) 2025 FieldingGroup
+Contact: h.h.fielding@ucl.ac.uk
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import Pkg
 Pkg.activate(".")
 
